571711137dd559368a7055c0365fe798d98cd7ec
Arini11 <elarnaumc@gmail.com>
2022-07-18 17:21:10 +0200

    menu, div() y mult()

--- src/ex4/Calculadora.java
+++ src/ex4/Calculadora.java
@@ -4,24 +4,94 @@ import javax.swing.JOptionPane;
 
 public class Calculadora {
 	public void start() {
-
-		String operador = JOptionPane.showInputDialog("Operacion:");
-		switch (operador) {
-			case "+":
-				break;
-			case "-":
-				break;
-			case "*":
-				break;
-			case "/":
-				break;
-			case "^2":
-				break;
-			case "^3":
-				break;
-			default:
-				System.out.println("Operador invalido");
-				break;
+		boolean loop = true;
+		
+		while(loop) {
+			String operador = JOptionPane.showInputDialog("Operaciones:\nsuma, resta, mult, div, cuadrado, cubo\n\n\"exit\" para salir");
+			switch (operador) {
+				case "suma":
+					suma();
+					break;
+				case "resta":
+					resta();
+					break;
+				case "mult":
+					mult();
+					break;
+				case "div":
+					div();
+					break;
+				case "cuadrado":
+					cuadrado();
+					break;
+				case "cubo":
+					cubo();
+					break;
+				case "salir":
+					loop = false;
+					break;
+				default:
+					JOptionPane.showMessageDialog(null, "Operador invalido");
+					break;
+			}
 		}
 	}
+
+	private void cubo() {
+		// TODO Auto-generated method stub
+	}
+
+	private void cuadrado() {
+		// TODO Auto-generated method stub
+	}
+	
+	private void resta() {
+		// TODO Auto-generated method stub
+	}
+
+	private void suma() {
+		// TODO Auto-generated method stub
+	}
+
+	private void div() {
+		String n1,n2;
+		do {
+			n1 = JOptionPane.showInputDialog(null, "Introduce el primer numero");
+		} while(!esNum(n1));
+		do {
+			n2 = JOptionPane.showInputDialog(null, "Introduce el segundo numero");
+		} while(!esNum(n2));
+		
+		if(Double.parseDouble(n1)==0 || Double.parseDouble(n2)==0) {
+			JOptionPane.showMessageDialog(null, "No se puede dividir por 0");
+			return;
+		}
+		
+		double resultado = Double.parseDouble(n1) / Double.parseDouble(n2);
+		JOptionPane.showMessageDialog(null, "Resultado: "+resultado);
+	}
+
+	private void mult() {
+		String n1,n2;
+		do {
+			n1 = JOptionPane.showInputDialog(null, "Introduce el primer numero");
+		} while(!esNum(n1));
+		do {
+			n2 = JOptionPane.showInputDialog(null, "Introduce el segundo numero");				
+		} while(!esNum(n2));
+		
+		double resultado = Double.parseDouble(n1) * Double.parseDouble(n2);
+		JOptionPane.showMessageDialog(null, "Resultado: "+resultado);
+	}
+	
+	public boolean esNum(String n){
+        try{  
+            Double.parseDouble(n);  
+            return true;
+        } 
+        catch(NumberFormatException e){
+        	JOptionPane.showMessageDialog(null, "Por favor, introduce un numero valido.");
+            return false;
+        }
+    }
 }
