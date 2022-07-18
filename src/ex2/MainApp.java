61026e7ad7425a864ea99655d1a54fe3c86bf6c9
Arini11 <elarnaumc@gmail.com>
2022-07-18 13:43:56 +0200

    crear clase excepcion y main

--- src/ex2/MainApp.java
+++ src/ex2/MainApp.java
@@ -1,9 +1,20 @@
 package ex2;
 
+import javax.swing.JOptionPane;
+
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int n = Integer.parseInt(JOptionPane.showInputDialog("Introduce código error"));
+		
+		try {
+			throw new ExcepcionCustomizada(n);
+		} catch (ExcepcionCustomizada e) {
+			System.out.println("Mensaje mostrado por pantalla."
+					+ "\nExcepcion capturada con mensaje: " + e.getMessage()
+					+ "\nPrograma terminado.");
+		}
 
 	}
 
