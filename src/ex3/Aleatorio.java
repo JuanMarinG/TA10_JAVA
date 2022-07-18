c692d8cf4b4b0b61d46684700e14527115bfc377
Joana <joanajanefdez@gmail.com>
2022-07-18 14:23:11 +0200

    creacion numero random

--- src/ex3/Aleatorio.java
+++ src/ex3/Aleatorio.java
@@ -1,8 +1,16 @@
 package ex3;
 
+import java.util.Random;
+
 public class Aleatorio {
 
-	public void start() {
+	public void start() {		
+		
+		int min = 0;
+        int max = 999;
+        
+        Random newRandom = new Random();
+        int random = newRandom.nextInt(max) + min;
 		
 	}
 }
