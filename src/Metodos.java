d3df56a6c6e60ec59c2e28a10de32349752fe3ee
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-07-18 10:04:44 +0200

    Simplificamos Mathrandom

--- src/Metodos.java
+++ src/Metodos.java
@@ -2,7 +2,7 @@
 public class Metodos {
 
 	public static int generarNumeroAleatorio() {
-        int num = (int)(Math.random()*(1-(500+1))+(500+1));
+        int num = (int)(Math.random()*(500+1));
         return num;}
 	
 }
