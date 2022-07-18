d054a123559cce25bdc9713846fe5aeb7609834d
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-07-18 14:42:45 +0200

    Generamos numero random, decimos si es par o impar y forzamos excepcion

--- src/ex3/Aleatorio.java
+++ src/ex3/Aleatorio.java
@@ -1,8 +1,24 @@
 package ex3;
 
+import java.io.IOException;
+
 public class Aleatorio {
 
 	public void start() {
-		
+
+		int random = 0;
+
+		try {
+
+			if (random % 2 == 0) {
+				 throw new Exception("ES PAR");
+            }else{
+            	throw new Exception("ES IMPAR");
+			}
+		} catch (Exception e) {
+
+			System.out.printf(e.getMessage());
+		}
 	}
+
 }
