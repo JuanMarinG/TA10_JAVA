829aa9203bb3e0996e0e780cc2dfdf59fe8c9441
Arini11 <elarnaumc@gmail.com>
2022-07-18 10:20:51 +0200

    main y setup

---
+++ src/ex1/MainApp.java
@@ -0,0 +1,11 @@
+package ex1;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		int rand = Metodos.generarNumeroAleatorio();
+
+		//Adivina.start();
+	}
+
+}
--- src/ex1/Metodos.java
+++ src/ex1/Metodos.java
@@ -1,8 +1,10 @@
+package ex1;
 
 public class Metodos {
 
 	public static int generarNumeroAleatorio() {
         int num = (int)(Math.random()*(500+1));
-        return num;}
+        return num;
+    }
 	
 }
