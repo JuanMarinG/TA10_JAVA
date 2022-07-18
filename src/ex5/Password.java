a3e836967df4570316284a6382458afddd63957c
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-07-19 00:32:35 +0200

    añadimos atributos, constructores, get and set y metodo generar pass

--- src/ex5/Password.java
+++ src/ex5/Password.java
@@ -4,30 +4,61 @@ public class Password {
 
 	// ATRIBUTS
 
+	String dic = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz" + "0123456789";
+	private int longitud = 0;
+	private String pass;
+
 	// CONSTRUCTORS
+
+	public Password(int longitud, String pass) {
+		super();
+		this.longitud = longitud;
+		this.pass = generarPassword(longitud, pass);
+	}
+
+	public Password() {
+		this.longitud = 8;
+	}
 
 	// GETTERS SETTERS
 
-	// METODE public String generarPassword(int longitud){}
+	public int getLongitud() {
+		return longitud;
+	}
+
+	public void setLongitud(int longitud) {
+		this.longitud = longitud;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	// Metodo generarPasswd
+	public String generarPassword(int longitud, String dic) {
+		String pswd = "";
+
+		for (int i = 0; i < longitud; i++) {
+			pswd += (dic.charAt((int) (Math.random() * dic.length())));
+		}
+		return pswd;
+	}
 
 	// METODE public boolean esFuerte(String passwd){}
 	public boolean esFuerte(String passwd) {
 		boolean forta = false;
 		// Comptador numeros, minuscules i majuscules
-		int nums=0, minus=0, majus=0;
-		// Diccionaris
-		String dicMinus = "abcdefghijklmnopqrstuvwxyz";
-		String dicMajus = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
+		int nums = 0, minus = 0, majus = 0;
 		// Comptar numeros, minuscules i majuscules
-		for (int i=0; i<passwd.length(); i++) {
+		for (int i = 0; i < passwd.length(); i++) {
 			try {
 				char c = passwd.charAt(i);
 				int compNum = Integer.parseInt(String.valueOf(c));
 				// Si peta a la linia anterior, no es un numero i no sumara res
 				nums++;
-			} catch (NumberFormatException e) {}
-			
+			} catch (NumberFormatException e) {
+			}
+
 			if ((passwd.charAt(i) < 91) && (passwd.charAt(i) > 64)) {
 				majus++;
 			} else if ((passwd.charAt(i) < 123) && (passwd.charAt(i) > 96)) {
