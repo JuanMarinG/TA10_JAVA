61cea910310805162f08ff7756e119d3b919f186
Arini11 <elarnaumc@gmail.com>
2022-07-18 10:33:13 +0200

    metodo setup

--- src/ex1/Adivina.java
+++ src/ex1/Adivina.java
@@ -1,6 +1,19 @@
 package ex1;
 
+import javax.swing.JOptionPane;
+
 public class Adivina {
 
+public static void start(int a) {
+		
+		int n = 0; // numero introducido por el usuario
+		int cont = 0; // contador de intentos
+		boolean loop = true; // estado del juego
+		
+		do {
+			// codigo
+		}while(loop);
+		
+	}
 
 }
