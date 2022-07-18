cb52b6959b11b44235c4260435f7d8c05a9154b5
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-07-18 14:08:39 +0200

    Creamos un constructor con excepcion y el override

--- src/excepcionCustom/ExcepcionCustomizada.java
+++ src/excepcionCustom/ExcepcionCustomizada.java
@@ -7,8 +7,30 @@ public class ExcepcionCustomizada extends Exception {
 	private final static String MSG2 = "Excepci�n Customizada 2";
 	private final static String MSG3 = "Excepci�n Customizada 3";
 	private final static String MSG_ERROR = "Excepci�n desconocida";
+	
+	// hacer constructores 1 vacio y 1 con codigoExcepcion
+	
+	public ExcepcionCustomizada(int codigoExcepcion) {
+		super();
+		this.codigoExcepcion = codigoExcepcion;
+	}
 
-	// hacer constructores 1 vac�o y 1 con codigoExcepcion
+	@Override
+	public String getMessage() {
+		// TODO Auto-generated method stub
+		
+		switch(codigoExcepcion) {
+		case 1:
+			return MSG1;
+		case 2:
+			return MSG2;
+		case 3:
+			return MSG3;
+		default:
+			return MSG_ERROR;
+		}
+	}
+
 	
 	// override de getMessage()
 
